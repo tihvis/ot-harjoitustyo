e2faec23ca6f2b295fda63eda76a2ba39daa1820
Mira <mira.tihverainen@gmail.com>
2024-03-13 12:40:03 +0200

    Tehtävä 3

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -8,7 +8,6 @@ class TestMaksukortti(unittest.TestCase):
     def test_konstruktori_asettaa_saldon_oikein(self):
         self.assertEqual(str(self.kortti), "Kortilla on rahaa 10.00 euroa")
 
-
     def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
         self.kortti.syo_edullisesti()
 
@@ -34,3 +33,29 @@ class TestMaksukortti(unittest.TestCase):
         self.kortti.lataa_rahaa(20000)
 
         self.assertEqual(self.kortti.saldo_euroina(), 150.0)
+
+    def test_syo_maukkaasti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(350)
+        kortti.syo_maukkaasti()
+
+        self.assertEqual(kortti.saldo_euroina(), 3.5)
+
+    def test_negatiivisen_summan_lataaminen_ei_muuta_kortin_saldoa(self):
+        self.kortti.lataa_rahaa(-100)
+
+        self.assertEqual(self.kortti.saldo_euroina(), 10.0)
+
+    def test_kortilla_voi_ostaa_edullisen_lounaan_kun_saldoa_edullisen_lounaan_verran(self):
+        kortti = Maksukortti(250)
+        kortti.syo_edullisesti()
+
+        self.assertEqual(kortti.saldo_euroina(), 0.0)
+
+    def test_kortilla_voi_ostaa_maukkaan_lounaan_kun_saldoa_maukkaan_lounaan_verran(self):
+        kortti = Maksukortti(400)
+        kortti.syo_maukkaasti()
+
+        self.assertEqual(kortti.saldo_euroina(), 0.0)
+
+    
+
