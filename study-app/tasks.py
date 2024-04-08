11c370cb0d19258d4d078d18cf9f0d1369254469
Mira <mira.tihverainen@gmail.com>
2024-04-08 13:21:40 +0300

    Pylint ja autopep8 lisätty riippuvuuksiin, ja niille luotu invoke komennot.

--- study-app/tasks.py
+++ study-app/tasks.py
@@ -7,10 +7,18 @@ def start(ctx):
 @task
 def build(ctx):
     ctx.run("python3 src/build.py", pty=True)
-    
+
 @task
 def test(ctx):
     ctx.run("pytest src", pty=True)
+
+@task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
+
+@task
+def format(ctx):
+    ctx.run("autopep8 --in-place --recursive src", pty=True)
 
 @task
 def coverage(ctx):
